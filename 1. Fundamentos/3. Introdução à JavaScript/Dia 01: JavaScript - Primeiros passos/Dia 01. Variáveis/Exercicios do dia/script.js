33daedf66dc09123221d74ae136534bcbd08414d
Analice Silva De Souza <analicesilvadsouza200@gmail.com>
2023-06-03 18:37:52 -0300

    exercicio 2 concluido

--- 1. Fundamentos/3. Introdução à JavaScript/Dia 01: JavaScript - Primeiros passos/Dia 01. Variáveis/Exercicios do dia/script.js
+++ 1. Fundamentos/3. Introdução à JavaScript/Dia 01: JavaScript - Primeiros passos/Dia 01. Variáveis/Exercicios do dia/script.js
@@ -22,7 +22,24 @@ let multiplicacao = num1 * num2;
 console.log(multiplicacao);
 
 let divisao = num1 / num2;
-console.log(divisao);
+console.log(divisao.toFixed(2));
 
 let modulo = num1 % num2;
 console.log(modulo);
+
+
+//Exercício 2
+//Utilize if/else para escrever um código que retorne o maior de dois números. Defina, no começo do seu código, duas variáveis com os valores que serão comparados.
+
+let valor1 = 19;
+let valor2 = 16;
+
+if(valor1 > valor2){
+    console.log(valor1 + 'é maior que ' + valor2);
+}
+else if(valor1 === valor2) {
+    console.log(valor1 + 'é igual' + valor2);
+}
+else{
+    console.log(valor2 + 'é maior que ' + valor1);
+}
