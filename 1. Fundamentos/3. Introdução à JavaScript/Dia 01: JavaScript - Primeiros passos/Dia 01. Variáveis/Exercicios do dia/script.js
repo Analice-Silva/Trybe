085553eba4561aeefbf263d7787d41edbd78e975
Analice Silva De Souza <analicesilvadsouza200@gmail.com>
2023-06-03 18:30:23 -0300

    realizando divisao exercicio 1

--- 1. Fundamentos/3. Introdução à JavaScript/Dia 01: JavaScript - Primeiros passos/Dia 01. Variáveis/Exercicios do dia/script.js
+++ 1. Fundamentos/3. Introdução à JavaScript/Dia 01: JavaScript - Primeiros passos/Dia 01. Variáveis/Exercicios do dia/script.js
@@ -18,5 +18,9 @@ console.log(adicao);
 let subtracao = num1 - num2;
 console.log(subtracao);
 
-let multiplicacao = num1 + num2;
+let multiplicacao = num1 * num2;
 console.log(multiplicacao);
+
+let divisao = num1 / num2;
+console.log(divisao);
+
