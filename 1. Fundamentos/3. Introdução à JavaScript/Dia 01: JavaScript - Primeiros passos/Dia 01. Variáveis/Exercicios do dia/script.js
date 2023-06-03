bcdeb28a799f3e4deb471ac76629c3a59cef9085
Analice Silva De Souza <analicesilvadsouza200@gmail.com>
2023-06-03 18:26:54 -0300

    criando as variaveis num1 e num2

---
+++ 1. Fundamentos/3. Introdução à JavaScript/Dia 01: JavaScript - Primeiros passos/Dia 01. Variáveis/Exercicios do dia/script.js
@@ -0,0 +1,13 @@
+//Nos exercícios de hoje, você vai elaborar alguns códigos de acordo com o que é pedido no enunciado. Todos os exercícios trabalham a lógica condicional (if...else e switch/case), os operadores aritméticos (+, -, *, /, %) e os operadores lógicos (>, <, &&, ||). Para executar seus códigos, recomendamos que utilize a extensão Code Runner.
+
+//Exercício 1
+//Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. É necessário que seu código tenha duas variáveis, num1 e num2, definidas no começo com os valores que serão operados. Escreva códigos para:
+
+//Adição (num1 + num2)
+//Subtração (num1 - num2)
+//Multiplicação (num1 * num2)
+//Divisão (num1 / num2)
+//Módulo (num1 % num2)
+
+let num1 = 7;
+let num2 = 9;
