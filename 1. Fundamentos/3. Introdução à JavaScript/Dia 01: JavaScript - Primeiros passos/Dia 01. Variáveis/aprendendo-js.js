e96f2805dc0219913b8ba74142ebfb17460554d2
Analice Silva De Souza <analicesilvadsouza200@gmail.com>
2023-06-02 21:18:47 -0300

    introducao ao Js

---
+++ 1. Fundamentos/3. Introdução à JavaScript/Dia 01: JavaScript - Primeiros passos/Dia 01. Variáveis/aprendendo-js.js
@@ -0,0 +1,42 @@
+
+/*let nomePessoa = "Enza";
+nomePessoa = "Rafaela";
+console.log(nomePessoa);
+ */
+
+
+/*if(true) {
+    let xablau1 = 'não vaza o escopo'
+    const xablau2 = 'não vaza o escopo'
+    var xablau3 = 'vaza o escopo' 
+}
+
+console.log(xablau3);*/
+
+/*let idade = 18;
+if (idade >= 18) {
+    console.log('Pode entrar!');
+}
+else{
+    console.log('Entrada proibida!')
+}
+
+idade = 18;
+if (idade >= 18) 
+    console.log('Pode entrar!');
+else
+    console.log('Entrada proibida!')
+*/
+
+/*
+let minhaVariavel = 8;
+console.log(typeof minhaVariavel);    
+*/
+/*
+let valor1 = 3;
+let valor2 = 3;
+console.log(valor1 === valor2);
+*/
+
+let variavel = true;
+console.log(!variavel);
