b73652af9629afb52fa5e87089b19e467a3db585
Analice Silva De Souza <analicesilvadsouza200@gmail.com>
2023-06-04 13:29:17 -0300

    concluido

--- 1. Fundamentos/3. Introdução à JavaScript/Dia 01: JavaScript - Primeiros passos/Dia 01. Variáveis/Exercicios do dia/script.js
+++ 1. Fundamentos/3. Introdução à JavaScript/Dia 01: JavaScript - Primeiros passos/Dia 01. Variáveis/Exercicios do dia/script.js
@@ -87,5 +87,123 @@ if(angulosValidos){
     console.log('Erro: ângulo inválido');
 }
 
+//Exercício 5
+//Utilize switch/case para escrever um código que receba o nome de uma peça de xadrez e retorne os movimentos que ela pode fazer.
 
+//Se a peça passada for inválida, o código deve retornar uma mensagem de erro.
+//⭐️ Desafio extra, escreva um código para funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais. Uma dica é pesquisar uma função que faça uma string ficar com todas as letras minúsculas (lower case).
 
+//Exemplo: Bispo -> Diagonais.
+let peca = 'bispo';
+let message = '';
+
+switch (peca.toLocaleLowerCase()) {
+    case 'rei':
+        message = 'Rei -> Uma casa para qualquer direção.';
+        break;
+    case 'bispo':
+        message = 'Bispo -> Diagonais.';
+        break;
+    case 'rainha':
+        message = 'Rainha -> Diagonal, horizontal e vertical.';
+        break;
+    case 'cavalo':
+        message = 'Cavalo -> "L" pode pular sobre as peças.';
+        break;
+    case 'torre':
+        message = 'Torre -> Horizontal e vertical.';
+        break;
+    case 'peão':
+        message = 'Peão -> Uma casa para frente, no primeiro movimento podem ser duas casas.';
+        break;
+    default:
+        message = 'Erro: peça inválida';
+}
+
+console.log(message);
+
+//🚀 Exercício 6: Há um par entre nós
+//6 - Utilize if...else para escrever um código que defina três números em variáveis e retorne true se pelo menos uma das três for par. Caso contrário, o código deve retornar false.
+//Faça esse exercício utilizando somente um if.
+
+let var1 = 12;
+let var2 = 1;
+let var3 = 9;
+
+if(var1 % 2 === 0 || var2 % 2 === 0 || var3 % 2 === 0) {
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+//Exercício 7
+//Utilize if...else para escrever um código que, dado um salário bruto, calcule o salário líquido a ser recebido.
+
+//Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS (Instituto Nacional do Seguro Social) e o IR (Imposto de Renda).
+
+//⭐️ A notação para um salário de R$1.500,10, por exemplo, deve ser 1500.10.
+
+//Para as faixas de impostos, use as seguintes referências:
+
+//INSS
+//Salário bruto até R$ 1.556,94: alíquota de 8%;
+//Salário bruto de R$ 1.556,95 a R$ 2.594,92: alíquota de 9%;
+//Salário bruto de R$ 2.594,93 a R$ 5.189,82: alíquota de 11%;
+//Salário bruto acima de R$ 5.189,82: alíquota máxima de R$ 570,88.
+//IR
+//Até R$ 1.903,98: isento de imposto de renda;
+//De R$ 1.903,99 a 2.826,65: alíquota de 7,5% e parcela de R$ 142,80 a deduzir do imposto;
+//De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto;
+//De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto;
+//Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.
+//O cálculo deve ser o demonstrado a seguir
+//O salário bruto está entre R$ 2.594,93 e R$ 5.189,82, então sua alíquota para o INSS é de 11%. O INSS será 11% de R$ 3.000, ou seja, R$ 330,00.
+
+//Para descobrir o salário-base, subtraia do salário bruto a alíquota do INSS: R$ 3.000,00 - R$ 330,00 = R$ 2.670,00.
+
+//Para calcular o valor do IR, considera-se um salário-base (já deduzido o INSS) entre R$ 1.903,99 e 2.826,65, em que a alíquota é de 7.5%, com parcela de R$ 142,80 a deduzir do imposto. Assim, tem-se:
+
+//R$ 2.670,00 - salário com INSS já deduzido;
+//7.5% - alíquota de imposto de renda, que representa um desconto de R$ 200,25;
+//R$ 142,80 - parcela a ser deduzida do imposto de renda.
+//Para obter o valor do imposto de renda, calcula-se: R$ 200,25 (que representa 7,5% de R$ 2.670,00) - R$ 142,80 (dedução do imposto de renda) = R$ 57,45.
+
+//Para obter o salário líquido, calcula-se: R$ 2.670,00 - R$ 57,45 (salário-base - valor IR) = R$ 2.612,55.
+
+//Resultado: R$ 2.612,55.
+
+//👀 Dica: Que tal identificar as alíquotas com variáveis de nomes explicativos?
+
+let salarioBruto = 3000;
+let aliquotaINSS;
+let aliquotaIR;
+
+if (salarioBruto <= 1556.94) {
+    aliquotaINSS = salarioBruto * (8 / 100);
+} else if (salarioBruto > 1556.94 && salarioBruto <= 2594.92) {
+    aliquotaINSS = salarioBruto * (9 / 100);
+} else if (salarioBruto > 2594.92 && salarioBruto <= 5189.82) {
+    aliquotaINSS = salarioBruto * (11 / 100);
+} else {
+    aliquotaINSS = 570.88
+}
+//console.log(aliquotaINSS)
+
+let salarioBase = salarioBruto - aliquotaINSS;
+//console.log(salarioBase.toFixed(2))
+
+if (salarioBase <= 1903.99) {
+    aliquotaIR = 0;
+} else if (salarioBase > 1903.99 && salarioBase <= 2826.65) {
+    aliquotaIR = salarioBase * (7.5 / 100) - 142.80;
+} else if (salarioBase > 2826.66 && salarioBase <= 3751.05) {
+    aliquotaIR = salarioBase * (15 / 100) - 354.80;
+} else if (salarioBase > 3751.06 && salarioBase <= 4664.68) {
+    aliquotaIR = salarioBase * (22.5 / 100) - 636.13;
+} else {
+    aliquotaIR = salarioBase * (27.5 / 100) - 869.36;
+}
+
+let salarioLiquido = salarioBase - aliquotaIR;
+
+console.log(salarioLiquido)
