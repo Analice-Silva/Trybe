f5641800deaf4c23a095bca5949f4b1013aec8af
Analice Silva De Souza <analicesilvadsouza200@gmail.com>
2023-06-06 20:16:42 -0300

    treinando for

---
+++ 1. Fundamentos/3. Introdução à JavaScript/Dia 02 :  JavaScript - Array e Loop For/treinando-loop.js
@@ -0,0 +1,21 @@
+/*
+const shopping = ['Sabão em pó', 'Macarrão', 'Leite condensado', 'Refrigerante', 'Maçã', 'Alface'];
+
+for (let index = 0; index < shopping.length; index += 1) {
+    //console.log(index)
+    const items = shopping[index];
+    console.log(`${index} - ${items}`);
+}
+*/
+
+//Exercício 1
+//Utilize o for para imprimir os elementos da lista groceryList através de console.log():
+/*
+const groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia', 'Macarrão'];
+
+for(let index = 0; index < groceryList.length; index +=1) {
+    let list = groceryList[index];
+    console.log(`${index} - ${list}`)
+}
+
+*/
