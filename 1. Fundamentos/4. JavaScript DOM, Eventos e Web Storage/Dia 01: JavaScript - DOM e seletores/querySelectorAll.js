f321af1a187857db8aaae6dde5971d12e2ecf4e4
Analice Silva De Souza <analicesilvadsouza200@gmail.com>
2023-06-14 12:21:07 -0300

    aulas

---
+++ 1. Fundamentos/4. JavaScript DOM, Eventos e Web Storage/Dia 01: JavaScript - DOM e seletores/querySelectorAll.js
@@ -0,0 +1,5 @@
+console.log(document.querySelectorAll('p')[0].innerText = 'lenda') //aqui alterei o primeiro paragrafo(Ayrton Senna) para lenda
+
+console.log(document.querySelectorAll('.piloto-f1-atual').length);
+
+console.log(document.querySelectorAll('.piloto-sem-titulo .piloto-f1-atual').length);
