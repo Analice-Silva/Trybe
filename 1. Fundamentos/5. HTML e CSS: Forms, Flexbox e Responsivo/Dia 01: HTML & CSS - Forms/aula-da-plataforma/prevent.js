bc6f9666c1ccc73c34c3b70fa95de4ac6b9dd315
Analice Silva De Souza <analicesilvadsouza200@gmail.com>
2023-06-22 10:10:47 -0300

    anula do dia

---
+++ 1. Fundamentos/5. HTML e CSS: Forms, Flexbox e Responsivo/Dia 01: HTML & CSS - Forms/aula-da-plataforma/prevent.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('#buttonSubmit');
+let inputNameUser = document.querySelector('#inputNameUser');
+const nameUser = document.querySelector('#nameUser');
+
+function getUserValue (event) {
+    event.preventDefault();
+    console.log(inputNameUser.value);
+    nameUser.innerHTML = inputNameUser.value;
+}
